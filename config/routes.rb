7a79d30517493c5e33011ded7a6b55c0f8bb9bc6
Daniel Casadio <danielcasadio@gmail.com>
2016-10-08 22:24:54 +0000

    Correção das páginas home e signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'static/home'
+  get 'static/signup'
+  
+  get 'home', :to => 'static#home'
+  get 'signup', :to => 'static#signup'
   root 'static#test'
+  
   
 end
