e17c9ee6ddbe847b068c25a7c78383bfc920f2a2
danielcasadio <danielcasadio@gmail.com>
2016-10-11 19:56:29 +0000

    Mudanças no bd

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,11 @@ class StaticController < ApplicationController
   
   def test
   end
+  
+  def home
+  end
+  
+  def signup
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
+  root 'static#test'
   get 'static/home'
   get 'static/signup'
-  
   get 'home', :to => 'static#home'
   get 'signup', :to => 'static#signup'
-  root 'static#test'
+  
   
   
 end
